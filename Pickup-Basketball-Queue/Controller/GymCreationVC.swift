bcd635eac38414ebb283c3505e57e264e28bdf8a
barteaga <barteaga@umich.edu>
2020-06-26 17:50:09 -0400

    gymbackend is all set up

--- Pickup-Basketball-Queue/Controller/GymCreationVC.swift
+++ Pickup-Basketball-Queue/Controller/GymCreationVC.swift
@@ -20,6 +20,31 @@ class GymCreationVC: UIViewController {
     }
 
     @IBAction func doneBtnPressed(_ sender: UIButton) {
-        
+        if let gym = gymNameTextField.text, let numCourts = Int(numCourtsTextField.text!) {
+            DataService.instance.saveGym(gymName: gym, numOfCourts: numCourts)
+            self.performSegue(withIdentifier: "GymCreationViewtoGymView", sender: nil)
+        }
+        else {
+            showAlert(title: "Error", message: "Please make sure you have entered a valid gym name and valid number of courts (e.g. 1, 2, 3)" )
+        }
+    }
+    
+    //if user touches return, keyboard goes away
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    //if user touches outside of keyboard, keyboard goes away
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    //takes a title and message and uses them to create and show a custom alert
+    func showAlert(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(okAction)
+        self.present(alertController, animated: true, completion: nil)
     }
 }
