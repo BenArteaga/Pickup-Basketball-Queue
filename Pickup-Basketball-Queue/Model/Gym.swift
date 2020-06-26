1154e9aec50b4f5d0dbc8f756e55858c26dfd003
barteaga <barteaga@umich.edu>
2020-06-26 16:33:36 -0400

    added Gym class

--- Pickup-Basketball-Queue/Model/Gym.swift
+++ Pickup-Basketball-Queue/Model/Gym.swift
@@ -9,5 +9,30 @@
 import Foundation
 
 class Gym {
+    //players will add gyms that they use to their home view
+    fileprivate var _gymName: String?
+    //number of courts is important because each court will have a seperate queue
+    fileprivate var _numOfCourts: Int?
+    
+    var gymName: String? {
+        return _gymName
+    }
+    
+    var numOfCourts: Int? {
+        return _numOfCourts
+    }
+    
+    //regular initializer
+    init(in_gymName: String?, in_numOfCourts: Int?) {
+        _gymName = in_gymName
+        _numOfCourts = in_numOfCourts
+    }
+    
+    //initializes from dictionary from Firebase backend
+    init(gymData: Dictionary<String, AnyObject>) {
+        _gymName = gymData["gym"] as? String
+        _numOfCourts = gymData["courtCount"] as? Int
+    }
+    
     
 }
