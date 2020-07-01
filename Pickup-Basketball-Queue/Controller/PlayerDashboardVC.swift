27c77fae66bc8102cf01b016979f8cb330d9253d
barteaga <barteaga@umich.edu>
2020-06-30 22:10:08 -0400

    Added UI for add gym view

--- Pickup-Basketball-Queue/Controller/PlayerDashboardVC.swift
+++ Pickup-Basketball-Queue/Controller/PlayerDashboardVC.swift
@@ -7,12 +7,37 @@
 //
 
 import UIKit
+import Firebase
 
-class PlayerDashboardVC: UIViewController {
+class PlayerDashboardVC: UIViewController, UITableViewDelegate {
 
+    @IBOutlet weak var addGymBtn: UIButton!
+    
+    //View of all the gyms that the player has added to their dashboard
+    @IBOutlet weak var playerGymsTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        addGymBtn.layer.cornerRadius = 10
     }
-
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 70
+    }
+    
+    @IBAction func addGymBtnPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func logOutBtnPressed(_ sender: UIButton) {
+        do {
+            try Firebase.Auth.auth().signOut()
+            AuthService.instance.isLoggedIn = false
+            performSegue(withIdentifier: "PlayerViewtoSignInView", sender: nil)
+        } catch {
+            print("An error occured signing out")
+        }
+    }
+    
 }
