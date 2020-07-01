61ac8a7b0362cfd9c693ed7456bd3cf1622d0ca3
barteaga <barteaga@umich.edu>
2020-06-30 22:22:19 -0400

    Added CourtOption UI

---
+++ Pickup-Basketball-Queue/Controller/AddGymVC.swift
@@ -0,0 +1,19 @@
+//
+//  AddGymVC.swift
+//  Pickup-Basketball-Queue
+//
+//  Created by Ben Arteaga on 6/30/20.
+//  Copyright © 2020 Ben Arteaga. All rights reserved.
+//
+
+import UIKit
+
+class AddGymVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ Pickup-Basketball-Queue/Controller/CourtOptionsVC.swift
@@ -0,0 +1,18 @@
+//
+//  CourtOptionsVC.swift
+//  Pickup-Basketball-Queue
+//
+//  Created by Ben Arteaga on 6/30/20.
+//  Copyright © 2020 Ben Arteaga. All rights reserved.
+//
+
+import UIKit
+
+class CourtOptionsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
